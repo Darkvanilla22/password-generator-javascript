a49b0fe29ab8e9f465d093d0353d3c2a6a6b9f76
Darkvanilla22 <Finsters@TPA-ITFC.swctpa.local>
2024-01-17 21:25:20 -0500

    Added variables to define character usage in password generation

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables for character usage in password generation
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var characters = "!@#$%^&*()_-+=";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
