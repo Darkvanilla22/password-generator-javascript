0734b0e2d5b06d24490973e2d64fdbebb148e635
Darkvanilla22 <Finsters@TPA-ITFC.swctpa.local>
2024-01-17 21:29:20 -0500

    Rewritten 'writePassword' to shorten function

--- script.js
+++ script.js
@@ -10,10 +10,7 @@ var characters = "!@#$%^&*()_-+=";
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  document.querySelector("#password").value = password;
 }
 
 // Add event listener to generate button
